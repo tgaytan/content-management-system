87dfd9384a1453e4e39c7035554f72077a641209
tgaytan <tgaytan14@utexas.edu>
2023-11-11 13:55:17 -0600

    created schema.sql and ran it successfully to create database and tables

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+);
